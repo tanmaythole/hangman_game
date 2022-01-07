9fccb6d6d4b04b41706e56402995b51b72c805e5
Tanmay Thole <tanmaythole@gmail.com>
2022-01-07 17:07:51 +0530

    Update game at server

--- game/views.py
+++ game/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 import random
+from django.http.response import JsonResponse
 
 from game.models import Game
 
@@ -21,8 +22,21 @@ def get_guessed_word(word):
     return guessed_word
 
 
+def update_guessed_word(word, guessed_word, inp):
+    new_guess = ""
+    if inp in word:
+        for i in range(len(word)):
+            if word[i] in inp:
+                new_guess += inp
+            else:
+                new_guess += guessed_word[i]
+    else:
+        return 0
+    return new_guess 
+
 
 def index(request):
+
     if request.method=='GET':
         word = get_word()
         guessed_word = get_guessed_word(word)
@@ -30,5 +44,27 @@ def index(request):
         game.save()
         game.image = 6 - game.lives
         return render(request, 'index.html', { 'game':game })
+        
     elif request.method=='POST':
-        return 
+        game = Game.objects.filter(id=request.POST['game_id'])[0]
+        inp = request.POST['inp'].lower()
+        new_guess = update_guessed_word(game.word, game.guessed_word, inp)
+
+        if new_guess == 0:
+            game.lives = game.lives - 1
+            if game.lives == 0 or game.lives<0:
+                game.status = "lose"
+            game.save()
+            return JsonResponse({"ans":False, "lives":game.lives, "status":game.status}, status=200)
+            
+        else:
+            game.guessed_word = new_guess
+
+            if new_guess.lower() == game.word.lower() and game.lives>0:
+                game.status = "win"
+            game.save()
+
+            return JsonResponse({"ans":True, "guessed_word":game.guessed_word, "status":game.status}, status=200)
+        
+        return JsonResponse({}, 401)
+        
