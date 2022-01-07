d1430fe9eccaf9e1b2599687ee601b5f6fb45215
Tanmay Thole <tanmaythole@gmail.com>
2022-01-07 14:39:20 +0530

    Game Model created

--- game/models.py
+++ game/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    word = models.CharField(max_length=20)
+    gussed = models.CharField(max_length=20)
+    status = models.CharField(max_length=10, default="ongoing")
+    lives = models.IntegerField(default=6)
--- game/views.py
+++ game/views.py
@@ -1,6 +1,8 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method=='GET':
+        return render(request, 'index.html')
+    elif request.method=='POST':
+        return 
